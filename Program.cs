58b544e7af88f1036dd094848e6ba72fb730bbda
Kathi Einzenberger <113719929+orange-panther@users.noreply.github.com>
2023-04-14 07:27:56 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -56,42 +56,14 @@ do
 if (!doAgain)
 {
     Console.WriteLine("Oh no, the marble is running in a loop! The program will end now.");
-    Environment.Exit(0);
 }
-Console.WriteLine($"segments: {segments}\nteleports: {teleports}");
+else
+{
+    Console.WriteLine($"segments: {segments}\nteleports: {teleports}");
+}
 
 bool IsValidMarbleRun(int position, int[] indexes)
 {
     if (indexes.Contains(position)) { return false; }
     else { return true; }
 }
-
-// int ChangeHexToDecimal(string hex)
-// {
-//     hex = hex.Substring(2);
-//     int pow = 0;
-//     double number = 0;
-
-//     for (int i = hex.Length - 1; i >= 0; i--)
-//     {
-//         int digit;
-//         if (char.IsLetter(hex[i]))
-//         {
-//             digit = hex[i] switch
-//             {
-//                 'a' => 10,
-//                 'b' => 11,
-//                 'c' => 12,
-//                 'd' => 13,
-//                 'e' => 14,
-//                 'f' => 15,
-//                 _ => 0
-//             };
-//         }
-//         else { digit = hex[i]; }
-
-//         number += digit * Math.Pow(16, pow);
-//         pow++;
-//     }
-//     return (int)number; //type-cast
-// }
